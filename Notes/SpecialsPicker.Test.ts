0887833b142dec160beb4f66d9380ee12c7933fc
Damien Underdue <damienunderdue@Damiens-MacBook-Pro.local>
2022-05-14 11:29:19 -0400

    This should work

---
+++ Notes/SpecialsPicker.Test.ts
@@ -0,0 +1,30 @@
+
+function getSpecial(weekday: string): string {}
+  }switch(weekday){
+    case'Monday':
+    return "Family Night, Kids eat Free(under 12 or 12 passing)";
+    break;
+    case'Tuesday':
+    return" Taco Tuesday 50% All Tacos";
+    break;
+    case'Wednesday':
+    return "Waffle Wednesday";
+    break;
+    case' Thursday':
+    return" Traditional Thursday";
+    break;
+    default:
+    return" enjoy your weekend with 30% ice cream";
+
+    console.log(getSpecial('Monday'));
+    console.log(getSpecial)('Tuesday'));
+    console.log(getSpecial(('Wednesday'));
+    console.log(getSpecial)('Thursday'));
+
+
+   
+    )''
+
+
+
+}
